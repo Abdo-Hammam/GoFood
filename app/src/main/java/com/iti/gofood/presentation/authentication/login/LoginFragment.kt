3d9fb00c96881b4e342fcab71d392d40f7f8ab8e
Abdo-Hammam <abdoabohammam232003@gmail.com>
2024-08-19 02:24:47 +0300

    create the login and registration screens

--- app/src/main/java/com/iti/gofood/presentation/authentication/login/LoginFragment.kt
+++ app/src/main/java/com/iti/gofood/presentation/authentication/login/LoginFragment.kt
@@ -1,14 +1,21 @@
 package com.iti.gofood.presentation.authentication.login
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.iti.gofood.R
 
 
 class LoginFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +23,27 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navToSignup(view)
+        }
+
+    private fun navToSignup(view: View){
+
+        val signUpText = view.findViewById<TextView>(R.id.signup_nav)
+        val signSpan = SpannableString(signUpText.text)
+
+        val signUp = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+            }
+        }
+        signSpan.setSpan(signUp,23,30, Spanned.SPAN_USER)
+        signUpText.text = signSpan
+        signUpText.movementMethod = LinkMovementMethod.getInstance()
+
     }
 
 }
--- app/src/main/java/com/iti/gofood/presentation/authentication/register/RegisterFragment.kt
+++ app/src/main/java/com/iti/gofood/presentation/authentication/register/RegisterFragment.kt
@@ -1,10 +1,16 @@
 package com.iti.gofood.presentation.authentication.register
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.iti.gofood.R
 
 
@@ -16,6 +22,29 @@ class RegisterFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navToLogin(view)
+    }
+
+    private fun navToLogin(view: View){
+
+        val loginText = view.findViewById<TextView>(R.id.login_nav)
+        val signSpan = SpannableString(loginText.text)
+
+        val signUp = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+            }
+        }
+
+        signSpan.setSpan(signUp,25,32, Spanned.SPAN_USER)
+        loginText.text = signSpan
+        loginText.movementMethod = LinkMovementMethod.getInstance()
+
     }
 
 }
