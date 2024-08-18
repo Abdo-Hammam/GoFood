3e0c6fb75d4accc8cf16f1b7d45fa9ed96387382
walids2003 <94900888+walids2003@users.noreply.github.com>
2024-08-18 14:26:30 +0200

    added retrofit client

--- app/src/main/java/com/iti/gofood/data/remotesource/retrofit/DummyClass4.kt
+++ app/src/main/java/com/iti/gofood/data/remotesource/retrofit/DummyClass4.kt
@@ -1,4 +1,31 @@
 package com.iti.gofood.data.remotesource.retrofit
 
-class DummyClass4 {
+import com.iti.gofood.data.remotesource.retrofit.models.Food
+import com.iti.gofood.data.remotesource.retrofit.models.response
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("random.php")
+    fun getaProduct(): Call<response>
+}
+
+object RetrofitClient {
+    private const val BASE_URL = "www.themealdb.com/api/json/v1/1/"
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY // Log request and response body
+    }
+
+    val apiService: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(OkHttpClient.Builder().addInterceptor(loggingInterceptor).build())
+            .build()
+            .create(ApiService::class.java)
+    }
 }
---
+++ app/src/main/java/com/iti/gofood/data/remotesource/retrofit/models/Food.kt
@@ -0,0 +1,15 @@
+package com.iti.gofood.data.remotesource.retrofit.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+data class response(val meals:List<Food>)
+
+@Parcelize
+data class Food(val idMeal:String,
+                val strMeal:String,
+                val strCategory:String,
+                val strArea:String,
+                val strInstructions:String,
+                val strMealThumb:String,
+                val strYoutube:String) : Parcelable
