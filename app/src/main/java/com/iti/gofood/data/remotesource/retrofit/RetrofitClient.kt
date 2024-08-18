be4c0ab2daa6eaf8d1fb48bac35a91cb391ba188
walids2003 <94900888+walids2003@users.noreply.github.com>
2024-08-18 16:39:34 +0200

    updated RetrofitClient

--- app/src/main/java/com/iti/gofood/data/remotesource/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/iti/gofood/data/remotesource/retrofit/RetrofitClient.kt
@@ -1,18 +1,9 @@
 package com.iti.gofood.data.remotesource.retrofit
 
-import com.iti.gofood.data.remotesource.retrofit.models.Food
-import com.iti.gofood.data.remotesource.retrofit.models.response
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("random.php")
-    fun getaProduct(): Call<response>
-}
 
 object RetrofitClient {
     private const val BASE_URL = "www.themealdb.com/api/json/v1/1/"
