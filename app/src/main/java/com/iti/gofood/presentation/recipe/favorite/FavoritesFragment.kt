8dbb716330f299fd98f4ced95b26a1cdb1f6223c
walids2003 <94900888+walids2003@users.noreply.github.com>
2024-08-20 20:37:50 +0200

    added list of favs , removeItemToFav() and addItemToFav()

--- app/src/main/java/com/iti/gofood/presentation/recipe/favorite/FavoritesFragment.kt
+++ app/src/main/java/com/iti/gofood/presentation/recipe/favorite/FavoritesFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.iti.gofood.R
 
 class FavoritesFragment : Fragment() {
+    private val myViewModel: MyViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,21 +25,18 @@ class FavoritesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
+        val textView: TextView = view.findViewById(R.id.myTextView)
+        var numOfItems = 0
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-
-        val items = listOf(
-            Item(R.drawable.ic_launcher_background, "Title 1", "Description 1"),
-            Item(R.drawable.ic_launcher_foreground, "Title 2", "Description 2"),
-            Item(R.drawable.ic_launcher_background, "Title 3", "Description 1"),
-            Item(R.drawable.ic_launcher_foreground, "Title 4", "Description 2"),
-            Item(R.drawable.ic_launcher_background, "Title 5", "Description 1"),
-            Item(R.drawable.ic_launcher_foreground, "Title 6", "Description 2"),
-            Item(R.drawable.ic_launcher_background, "Title 7", "Description 1"),
-            Item(R.drawable.ic_launcher_foreground, "Title 8", "Description 2"),
-            Item(R.drawable.ic_launcher_background, "Title 9", "Description 1"),
-            Item(R.drawable.ic_launcher_foreground, "Title 10", "Description 2")
-            // Add more items here
-        )
-        recyclerView.adapter = ItemAdapter(items)
+        myViewModel.items.observe(viewLifecycleOwner) { data ->
+            numOfItems = data.size
+            recyclerView.adapter = ItemAdapter(data) // Update UI when data changes
+        }
+        if(numOfItems != 0) {
+            textView.visibility = View.GONE
+        }
+        else {
+            textView.visibility = View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/iti/gofood/presentation/recipe/favorite/ItemAdapter.kt
+++ app/src/main/java/com/iti/gofood/presentation/recipe/favorite/ItemAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.iti.gofood.R
 
