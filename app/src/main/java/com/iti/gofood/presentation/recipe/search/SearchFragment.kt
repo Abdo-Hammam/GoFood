c9c04480e8c8f0abeb537514010b08fbb080fa80
DELL <shymanasr273@gmail.com>
2024-08-24 13:38:04 -0700

    AddedSearchScreen

--- app/src/main/java/com/iti/gofood/presentation/recipe/search/SearchFragment.kt
+++ app/src/main/java/com/iti/gofood/presentation/recipe/search/SearchFragment.kt
@@ -5,17 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.iti.gofood.R
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.viewModels
+import com.example.project_iti.data.Remote.Retrofit.MyRetrofit
+import com.example.project_iti.data.Remote.source.RemoteDataSourceImpl
+import com.example.project_iti.presentation.ui.adapters.AdapterAllMeals
+import com.example.project_iti.presentation.ui.search.SearchViewModel
+import com.example.project_iti.presentation.ui.search.SearchViewModelFactory
+import com.example.project_iti.repo.Repo
+import com.iti.gofood.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
 
+    private lateinit var binding: FragmentSearchBinding
+    private val viewModel: SearchViewModel by viewModels { SearchViewModelFactory(Repo(
+        RemoteDataSourceImpl(MyRetrofit.service)
+    )) }
+    private val adapter: AdapterAllMeals by lazy { AdapterAllMeals(emptyList()) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.view1.adapter = adapter
+
+        viewModel.mealList.observe(viewLifecycleOwner) {
+            adapter.update(it)
+        }
+
+        binding.searchBox.addTextChangedListener { text ->
+            viewModel.searchMeals(text.toString())
+        }
+    }
 }
